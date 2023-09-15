313a8d13d6fdea5929659e198af087e04749f44c
min5407 <gmy5407@gmail.com>
2023-09-15 23:59:56 +0900

    style: add media query  for accordion

--- src/components/forecast/Accordion.tsx
+++ src/components/forecast/Accordion.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { WeatherIcon } from "../weatherIcon/WeatherIcon";
 import Styles from "./style.module.css";
+import Image from "next/image";
+import classNames from "classnames";
 
 export const Accordion = () => {
   const [isShow, setIsShow] = useState(false);
@@ -12,18 +14,26 @@ export const Accordion = () => {
     <div className={Styles.accordion}>
       <button className={Styles.accordionBtn} onClick={handleClick}>
         May 25
-        <span className={Styles.arrow} />
+        <Image
+          src="/arrow.png"
+          width={24}
+          height={24}
+          alt="arrow"
+          className={classNames(Styles.arrow, { [Styles.arrowDown]: !isShow })}
+        />
       </button>
       {isShow && (
-        <section className={Styles.dayInfo}>
-          <WeatherIcon size="small" />
-          <p className={Styles.time}>03:00 pm</p>
+        <>
+          <section className={Styles.dayInfo}>
+            <WeatherIcon size="small" />
+            <p className={Styles.time}>03:00 pm</p>
 
-          <div>
-            <p className={Styles.temperatureDesc}>clear Sky</p>
-            <p className={Styles.temperature}>297.32℃ / 297.32℃</p>
-          </div>
-        </section>
+            <div>
+              <p className={Styles.temperatureDesc}>clear Sky</p>
+              <p className={Styles.temperature}>297.32℃ / 297.32℃</p>
+            </div>
+          </section>
+        </>
       )}
     </div>
   );
